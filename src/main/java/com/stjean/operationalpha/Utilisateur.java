a9022ca36eb524bc4bd0cd2cb33a4ef26d3ded64
Sims08 <gillsimo08@gmail.com>
2025-11-11 21:31:18 +0100

    (feat): Creation de la classe utilisateur

---
+++ src/main/java/com/stjean/operationalpha/Utilisateur.java
@@ -0,0 +1,17 @@
+package com.stjean.operationalpha;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Utilisateur {
+    private int id;
+    private String nom;
+    private int age;
+    private String email;
+    private String telephone;
+    private String ville;
+    private double soldePersonnel;
+
+
+
+}
