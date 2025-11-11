c9495e23bf0d7caa9c92b3d0ab86ea2ff01c285d
Sims08 <gillsimo08@gmail.com>
2025-11-11 21:34:45 +0100

    (feat): Ajout des differentes methodes

--- src/main/java/com/stjean/operationalpha/Utilisateur.java
+++ src/main/java/com/stjean/operationalpha/Utilisateur.java
@@ -12,6 +12,72 @@ public class Utilisateur {
     private String ville;
     private double soldePersonnel;
 
+    public static ArrayList<Utilisateur> users = new ArrayList<>();
 
+ 
+    public Utilisateur(int id, String nom, int age, String email, String telephone, String ville, double soldePersonnel) {
+        this.id = id;
+        this.nom = nom;
+        this.age = age;
+        this.email = email;
+        this.telephone = telephone;
+        this.ville = ville;
+        this.soldePersonnel = soldePersonnel;
+    }
 
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public int getAge() { return age; }
+    public String getEmail() { return email; }
+    public String getTelephone() { return telephone; }
+    public String getVille() { return ville; }
+    public double getSoldePersonnel() { return soldePersonnel; }
+
+    public void setNom(String nom) { this.nom = nom; }
+    public void setAge(int age) { this.age = age; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+    public void setVille(String ville) { this.ville = ville; }
+    public void setSoldePersonnel(double soldePersonnel) { this.soldePersonnel = soldePersonnel; }
+
+    public static void ajouter(Utilisateur user) {
+        
+        users.add(user);
+        System.out.println("✅ Utilisateur ajouté : " + user.getNom());
+    }
+
+    public static void supprimer(int id) {
+        Utilisateur u = null;
+        for (Utilisateur user : users) {
+            if (user.getId() == id) {
+                u = user;
+                break;
+            }
+        }
+        users.remove(u);
+        System.out.println("🗑️ Utilisateur supprimé : " + u.getNom());
+    }
+
+    public static void lister() {
+        if (users.isEmpty()) {
+            System.out.println("Aucun utilisateur enregistré.");
+        } else {
+            System.out.println("📋 Liste des utilisateurs :");
+            for (Utilisateur u : users) {
+                System.out.println(u);
+            }
+        }
+    }
+
+    public static void afficher(int id) {
+        for (Utilisateur u : users) {
+            if (u.getId() == id) {
+                System.out.println("👤 Détails utilisateur : " + u);
+                return;
+            }
+        }
+        System.out.println("⚠️ Aucun utilisateur trouvé avec l'ID " + id);
+    }
+
+   
 }
