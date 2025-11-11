51914250646a3542e1ba282fbab7416bdc8deaf5
Sims08 <gillsimo08@gmail.com>
2025-11-11 19:30:19 +0100

    Initialisatiion du Projet

---
+++ src/main/java/com/stjean/TPTESTU1_SIMO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.stjean;
+
+/**
+ *
+ * @author gills
+ */
+public class TPTESTU1_SIMO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
