e7d6c09803829913fd56140cf38e63e069a43d92
Sims08 <gillsimo08@gmail.com>
2025-11-11 19:55:09 +0100

    (feat):Creation de la methode factoriel

---
+++ src/main/java/com/stjean/operation/FactorielInvalidException.java
@@ -0,0 +1,9 @@
+package com.stjean.operation;
+
+public class FactorielInvalidException extends Exception{
+	
+	public FactorielInvalidException(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -1,7 +1,16 @@
 package com.stjean.operation;
 
 public class OperationMathematique {
- boolean  estPositif(int nombre) {
+
+public static boolean  estPositif(int nombre) {
 	 return nombre>0;
  }
+ 
+ public static long factoriel(long a) throws FactorielInvalidException {
+		if(a<0)
+			throw new FactorielInvalidException("il faut un nombre positif");
+		if(a==0 || a==1) 
+			return 1;
+		return a*factoriel(a-1);
+	}
 }
